8457dd80ca6bc90ae1d6914d8318dbf38bb8a6a9
McDimas2005 <mcdimas2005@gmail.com>
2026-04-30 23:49:54 +0700

    chore: adding tag dicoding metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,14 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MitraScore AI",
-  description: "From informal business evidence to explainable credit readiness."
+  description: "From informal business evidence to explainable credit readiness.",
+  other: {
+    "dicoding:email": [
+      "michael.chrispradipta@binus.ac.id",
+      "bryan.naufal1@gmail.com",
+      "nkhanifah525@gmail.com"
+    ]
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
